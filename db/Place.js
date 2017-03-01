d6a08700c74df8acea1fd051ef14bb479d0c2206
rivermanvr <vrgithub@riversconsulting.com>
2017-03-01 18:00:44 -0500

    Models: HOTEL & Place

--- db/Place.js
+++ db/Place.js
@@ -5,7 +5,7 @@ const Place = tripDB.define('place', {
     city: tripDB.Sequelize.STRING,
     state: tripDB.Sequelize.STRING,
     phone: tripDB.Sequelize.STRING,
-    location: tripDB.Sequelize.ARRAY
+    location: tripDB.Sequelize.ARRAY(tripDB.Sequelize.FLOAT)
 });
 
 module.exports = Place;
