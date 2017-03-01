ec28570b72a77a9517c52ce457e0e063b8f3284b
rivermanvr <vrgithub@riversconsulting.com>
2017-03-01 15:59:51 -0500

    added a few items to server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require ( 'express' );
+const express = require( 'express' );
 const server = express();
 const tripDB = require( './db' );
 const methodOverride = require( 'method-override' );
 const bodyParser = require( 'body-parser' );
 const path = require( 'path' );
-const swig = require ( 'swig' );
+const swig = require( 'swig' );
 
 swig.setDefaults({ cache: false });
 server.set( 'view engine', 'html' );
-server.engine( "html", swig.renderFile );
+server.engine( 'html', swig.renderFile );
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(methodOverride( '_method' ))
@@ -16,10 +16,11 @@ server.use(methodOverride( '_method' ))
 server.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 server.use('/css', express.static(path.join(__dirname, './css')));
 
+tripDB.seed()
+    .then(() => console.log('your data is seeded'))
+    .catch( err => console.log(err));
+
+console.log('Models: ', tripDB.models);
+
 const port = process.env.PORT || 3000;
-
-
-
-
-
-app.listen(port,()=>console.log(`I am listening on ${port}`));
+server.listen(port, () => console.log(`listening on port ${port}`));
