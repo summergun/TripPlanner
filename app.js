3233d56be56e62a8aba17b885b2c493d64d83742
rivermanvr <vrgithub@riversconsulting.com>
2017-03-01 16:47:59 -0500

    split server.js into app & server files.

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require( 'express' );
+const app = express();
+const tripDB = require( './db' );
+const methodOverride = require( 'method-override' );
+const bodyParser = require( 'body-parser' );
+const path = require( 'path' );
+const swig = require( 'swig' );
+
+const models = tripDB.models;
+//..............................................
+// don't need these yet...uncomment as needed...
+// const Associations = models.Associations;
+// const Restaurant = models.Restaurant;
+// const Activity = models.Activity;
+// const Hotel = models.Hotel;
+// const Place = models.Place;
+//..............................................
+
+swig.setDefaults({ cache: false });
+app.set( 'view engine', 'html' );
+app.engine( 'html', swig.renderFile );
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride( '_method' ))
+
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+app.use('/css', express.static(path.join(__dirname, './css')));
+//..............................................
+//........need to setup specific routes.........
+//..............................................
+
+app.get('/', (req, res, next) => {
+    res.render('index')
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,5 @@
-const express = require( 'express' );
-const server = express();
+const server = require( 'http' ).createServer(require( './app' ));
 const tripDB = require( './db' );
-const methodOverride = require( 'method-override' );
-const bodyParser = require( 'body-parser' );
-const path = require( 'path' );
-const swig = require( 'swig' );
-
-swig.setDefaults({ cache: false });
-server.set( 'view engine', 'html' );
-server.engine( 'html', swig.renderFile );
-
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(methodOverride( '_method' ))
-
-server.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
-server.use('/css', express.static(path.join(__dirname, './css')));
-
-server.get('/', (req, res, next) => {
-    res.render('index')
-});
 
 tripDB.seed()
     .then(() => console.log('your data is seeded'))
